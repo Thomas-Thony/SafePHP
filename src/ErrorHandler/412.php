8fd52dd0a729b05c19e5bdbb347329002f4f893f
Thomas.Th <thomas.thony.69@gmail.com>
2026-03-14 09:37:10 +0100

    [NEW] Ajout de nouvelles erreurs http en controle d'erreur

---
+++ src/ErrorHandler/412.php
@@ -0,0 +1,3 @@
+<div class="error-container">
+    <h2 class="error-title">Precondition Failed</h2>
+</div>
---
+++ src/ErrorHandler/422.php
@@ -0,0 +1,3 @@
+<div class="error-container">
+    <h2 class="error-title">Unprocessable Entity</h2>
+</div>
